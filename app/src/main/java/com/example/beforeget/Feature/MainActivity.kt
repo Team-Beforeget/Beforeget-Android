70fa1284cb644b8c63209612c31ecaa0b26ee3c4
s9hn <tnghqn@gmail.com>
2022-01-05 17:59:18 +0900

    Welcome!

---
+++ app/src/main/java/com/example/beforeget/Feature/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.beforeget.Feature
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.beforeget.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
