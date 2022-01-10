e76c0c34055d7102cc4ef12a90a8be44edf606d4
hjh1161514 <hjh1161514@naver.com>
2022-01-10 23:21:59 +0900

    [FEAT]:Report layout
    1. 통계 전체뷰 구현
    2. 통계 viewpager, indicator 구현

---
+++ app/src/main/java/beforeget/feature/report/ReportActivity.kt
@@ -0,0 +1,42 @@
+package beforeget.feature.report
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.beforeget.databinding.ActivityReportBinding
+import com.google.android.material.tabs.TabLayoutMediator
+
+class ReportActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityReportBinding
+    private lateinit var reportViewPagerAdapter: ReportViewPagerAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityReportBinding.inflate(layoutInflater)
+
+        initAdapter()
+
+        setContentView(binding.root)
+    }
+
+    private fun initAdapter() {
+        val fragmentList = listOf(
+            ReportLabelingFragment(),
+            ReportGraphFragment(),
+            ReportRankingFragment(),
+            ReportSentenceFragment(),
+            ReportOnepageFragment()
+        )
+
+        reportViewPagerAdapter = ReportViewPagerAdapter(this)
+        reportViewPagerAdapter.fragments.addAll(fragmentList)
+
+        binding.vpReport.adapter = reportViewPagerAdapter
+
+        initIndicator()
+    }
+
+    private fun initIndicator() {
+        TabLayoutMediator(binding.tlReport, binding.vpReport) { _tab, _position ->
+        }.attach()
+    }
+}
