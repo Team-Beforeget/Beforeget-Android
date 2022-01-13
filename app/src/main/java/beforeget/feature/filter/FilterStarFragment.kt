a8d64c7301a9cd6f22551ad9e844e37cd5b74416
ssonghyun101 <songhyun0868@gmail.com>
2022-01-13 17:30:05 +0900

    [Feat] 필터 별점뷰 버튼 활성화 기능 구현 1. 별점 하나 선택시 적용 버튼 활성화 2. 별점 버튼 클릭 시 이벤트에 따른 색깔 변경 적용 Ref : #27 Related to : #1, #12 close #27

--- app/src/main/java/beforeget/feature/filter/FilterStarFragment.kt
+++ app/src/main/java/beforeget/feature/filter/FilterStarFragment.kt
@@ -1,7 +1,6 @@
 package beforeget.feature.filter
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,8 +16,41 @@ class FilterStarFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_filter_star, container,false)
-        Log.d("별점뷰","실행됨")
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_filter_star, container, false)
+        binding.btnApplyStarFilter.isEnabled = false
+        initClickEvent()
         return binding.root
+    }
+    private fun initClickEvent() {
+        binding.apply {
+            btnOneStar.setOnClickListener {
+                btnOneStar.isSelected = !btnOneStar.isSelected
+                activateBtnApplyFilterStarEnabled()
+            }
+            btnTwoStar.setOnClickListener {
+                btnTwoStar.isSelected = !btnTwoStar.isSelected
+                activateBtnApplyFilterStarEnabled()
+            }
+            btnThreeStar.setOnClickListener {
+                btnThreeStar.isSelected = !btnThreeStar.isSelected
+                activateBtnApplyFilterStarEnabled()
+            }
+            btnFourStar.setOnClickListener {
+                btnFourStar.isSelected = !btnFourStar.isSelected
+                activateBtnApplyFilterStarEnabled()
+            }
+            btnFiveStar.setOnClickListener {
+                btnFiveStar.isSelected = !btnFiveStar.isSelected
+                activateBtnApplyFilterStarEnabled()
+            }
+        }
+    }
+    private fun checkBtnStarSelected() = binding.btnOneStar.isSelected || binding.btnTwoStar.isSelected || binding.btnThreeStar.isSelected || binding.btnFourStar.isSelected || binding.btnFiveStar.isSelected
+    private fun checkBtnApplyFilterStarEnabled() {
+        binding.btnApplyStarFilter.isEnabled = checkBtnStarSelected()
+    }
+    private fun activateBtnApplyFilterStarEnabled() {
+        checkBtnStarSelected()
+        checkBtnApplyFilterStarEnabled()
     }
 }
