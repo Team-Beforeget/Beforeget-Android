cb0f4734a5ab48d3e2ce6b72191de447c157004e
s9hn <tnghqn@gmail.com>
2022-01-14 18:31:51 +0900

    [Feat]: 유형선택뷰 기능 구현 1. 클릭리스너 구현 중 2. ...

--- app/src/main/java/beforeget/feature/write/MediaSelectActivity.kt
+++ app/src/main/java/beforeget/feature/write/MediaSelectActivity.kt
@@ -1,12 +1,31 @@
 package beforeget.feature.write
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.example.beforeget.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.beforeget.databinding.ActivityMediaSelectBinding
 
 class MediaSelectActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMediaSelectBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_media_select)
+        binding = ActivityMediaSelectBinding.inflate(layoutInflater)
+
+        binding.clMediaBook.setOnClickListener {
+            initClickEvent()
+            binding.clMediaBook.isSelected = true}
+        binding.clMediaMovie.setOnClickListener { initClickEvent() }
+        binding.clMediaMusic.setOnClickListener { initClickEvent() }
+        binding.clMediaTv.setOnClickListener { initClickEvent() }
+        binding.clMediaWebtoon.setOnClickListener { initClickEvent() }
+        binding.clMediaYoutube.setOnClickListener { initClickEvent() }
+        binding.ivMediaClose.setOnClickListener { finish() }
+        binding.btnMediaNext.setOnClickListener { initClickEvent() }
+        setContentView(binding.root)
+    }
+
+    private fun initClickEvent() {
+        startActivity(Intent(this, WriteActivity::class.java))
     }
 }
