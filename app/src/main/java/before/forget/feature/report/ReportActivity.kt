a6e0133424aef8e46281efa5ff87fc7db12a0e55
hjh1161514 <hjh1161514@naver.com>
2022-01-16 01:50:07 +0900

    [FEAT] Report Labeling Inflate
    1. Max, Min Year, Month 설정
    2. Picker 초기 날짜 선택
    3. Picker 선택시 text와 picker setting 변경

--- app/src/main/java/before/forget/feature/report/ReportActivity.kt
+++ app/src/main/java/before/forget/feature/report/ReportActivity.kt
@@ -2,27 +2,27 @@ package before.forget.feature.report
 
 import android.app.AlertDialog
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import before.forget.databinding.ActivityReportBinding
-import com.google.android.material.tabs.TabLayoutMediator
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
 import android.widget.NumberPicker
+import androidx.appcompat.app.AppCompatActivity
 import before.forget.R
+import before.forget.databinding.ActivityReportBinding
+import com.google.android.material.tabs.TabLayoutMediator
 import java.util.*
-
 
 class ReportActivity : AppCompatActivity() {
     private lateinit var binding: ActivityReportBinding
     private lateinit var reportViewPagerAdapter: ReportViewPagerAdapter
 
     private val cal: Calendar = Calendar.getInstance()
-    private val reportYear = cal.get(Calendar.YEAR)
-    private val reportMonth = cal.get(Calendar.MONTH)
-    // TODO: 서버에서 값 받아오기
-    private var firstRecordYear = 0
-    private var firstRecordMonth = 0
+    private var currentYear = cal.get(Calendar.YEAR)
+    private var currentMonth = cal.get(Calendar.MONTH) + 1
+    private var selectYear = currentYear
+    private var selectMonth = currentMonth
+    private var IS_FIRST = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,26 +50,40 @@ class ReportActivity : AppCompatActivity() {
     private fun setDatePicker() {
         val dialog = AlertDialog.Builder(this).create()
         val view: View = LayoutInflater.from(this).inflate(R.layout.layout_report_datepicker_dialog, null)
-
         val year: NumberPicker = view.findViewById(R.id.picker_year)
         val month: NumberPicker = view.findViewById(R.id.picker_month)
         val cancel: Button = view.findViewById(R.id.picker_cancel)
         val confirm: Button = view.findViewById(R.id.picker_confirm)
 
-        year.minValue = firstRecordYear
-        year.maxValue = reportYear
-        month.minValue = 1
-        month.maxValue = 12
-
-        // setting current year and month
-        month.value = cal.get(Calendar.MONTH) + 1
-        year.value = cal.get(Calendar.YEAR)
+        // TODO: 서버에서 값 받아오기
+        var firstRecordYear = 2018
+        var firstRecordMonth = 0
 
         // release edittext setting
         year.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
         month.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
         year.wrapSelectorWheel = false
 
+        month.minValue = 1
+        month.maxValue = 12
+
+        // setting current year and month
+        if (IS_FIRST) {
+            if (currentMonth == 1) {
+                year.minValue = firstRecordYear
+                year.maxValue = currentYear - 1
+                currentYear = currentYear - 1
+                currentMonth = 12
+                year.value = currentYear
+                month.value = currentMonth
+            }
+        } else {
+            year.minValue = firstRecordYear
+            year.maxValue = currentYear
+            year.value = selectYear
+            month.value = selectMonth
+        }
+        IS_FIRST = false
         cancel.setOnClickListener {
             dialog.dismiss()
             dialog.cancel()
@@ -77,6 +91,9 @@ class ReportActivity : AppCompatActivity() {
 
         confirm.setOnClickListener {
             // TODO: date 전달
+            selectMonth = month.value
+            selectYear = year.value
+            binding.btnDatePicker.text = selectYear.toString() + "년 " + selectMonth.toString() + "월"
             dialog.dismiss()
             dialog.cancel()
         }
