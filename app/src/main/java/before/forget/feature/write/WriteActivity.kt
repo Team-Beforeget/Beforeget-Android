dab43c7e73d39d276f5af16954d352e095fa99ab
s9hn <tnghqn@gmail.com>
2022-01-17 06:10:22 +0900

    [Feat]:글쓰기뷰 기능 구현 1. 데이트 피커 구현 2. 별점 기능 구현

--- app/src/main/java/before/forget/feature/write/WriteActivity.kt
+++ app/src/main/java/before/forget/feature/write/WriteActivity.kt
@@ -1,20 +1,129 @@
 package before.forget.feature.write
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import before.forget.databinding.ActivityWriteBinding
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class WriteActivity : AppCompatActivity() {
+    var dateString = ""
+
+    @RequiresApi(Build.VERSION_CODES.N)
+
     private lateinit var binding: ActivityWriteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWriteBinding.inflate(layoutInflater)
-        binding.ivWriteBackbtn.setOnClickListener { finish() }
+
         setContentView(binding.root)
-        test()
+
+        val cal = Calendar.getInstance()
+        var day = ""
+        var num = cal.get(Calendar.DAY_OF_WEEK)
+        when (num) {
+            1 -> day = ". SUN"
+            2 -> day = ". MON"
+            3 -> day = ". TUE"
+            4 -> day = ". WED"
+            5 -> day = ". THU"
+            6 -> day = ". FRI"
+            7 -> day = ". SAT"
+        }
+
+        binding.tvWriteDatepickerbtn.text =
+            LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy. MM. dd")) + day
+        getMediaLabel()
+        btnSetOnClickListener()
     }
 
-    private fun test() {
+    private fun btnSetOnClickListener() {
+        with(binding) {
+            ivWriteStar1.setOnClickListener {
+                ivWriteStar1.isSelected = true
+                ivWriteStar2.isSelected = false
+                ivWriteStar3.isSelected = false
+                ivWriteStar4.isSelected = false
+                ivWriteStar5.isSelected = false
+            }
+            ivWriteStar2.setOnClickListener {
+                ivWriteStar1.isSelected = true
+                ivWriteStar2.isSelected = true
+                ivWriteStar3.isSelected = false
+                ivWriteStar4.isSelected = false
+                ivWriteStar5.isSelected = false
+            }
+            ivWriteStar3.setOnClickListener {
+                ivWriteStar1.isSelected = true
+                ivWriteStar2.isSelected = true
+                ivWriteStar3.isSelected = true
+                ivWriteStar4.isSelected = false
+                ivWriteStar5.isSelected = false
+            }
+            ivWriteStar4.setOnClickListener {
+                ivWriteStar1.isSelected = true
+                ivWriteStar2.isSelected = true
+                ivWriteStar3.isSelected = true
+                ivWriteStar4.isSelected = true
+                ivWriteStar5.isSelected = false
+            }
+            ivWriteStar5.setOnClickListener {
+                ivWriteStar1.isSelected = true
+                ivWriteStar2.isSelected = true
+                ivWriteStar3.isSelected = true
+                ivWriteStar4.isSelected = true
+                ivWriteStar5.isSelected = true
+            }
+            tvWriteDatepickerbtn.setOnClickListener {
+                val cal = Calendar.getInstance()
+                var day = ""
+
+                val dateSetListener =
+                    DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                        cal.set(year, month, dayOfMonth)
+
+                        if (month < 9) {
+                            var months = "0" + (month + 1)
+                            if (dayOfMonth < 10) {
+                                var dayofmonths = "0" + dayOfMonth
+                                dateString = "$year. $months. $dayofmonths"
+                            } else {
+                                dateString = "$year. $months. $dayOfMonth"
+                            }
+                        } else {
+                            dateString = "$year. ${month + 1}. $dayOfMonth"
+                        }
+
+                        var num = cal.get(Calendar.DAY_OF_WEEK)
+                        when (num) {
+                            1 -> day = ". SUN"
+                            2 -> day = ". MON"
+                            3 -> day = ". TUE"
+                            4 -> day = ". WED"
+                            5 -> day = ". THU"
+                            6 -> day = ". FRI"
+                            7 -> day = ". SAT"
+                        }
+                        tvWriteDatepickerbtn.text = dateString + day
+                    }
+
+                DatePickerDialog(
+                    this@WriteActivity,
+                    dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+            ivWriteBackbtn.setOnClickListener { finish() }
+        }
+    }
+
+    private fun getMediaLabel() {
         if (intent.hasExtra("media")) {
             val media = intent.getStringExtra("media")
             binding.tvWriteMedialabel.text = media.toString()
--- app/src/main/java/before/forget/feature/write/WriteBottomSheetFragment.kt
+++ app/src/main/java/before/forget/feature/write/WriteBottomSheetFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [OneLineReviewFragment.newInstance] factory method to
+ * Use the [WriteBottomSheetFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class OneLineReviewFragment : Fragment() {
+class WriteBottomSheetFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,7 +36,7 @@ class OneLineReviewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_one_line_review, container, false)
+        return inflater.inflate(R.layout.fragment_write_bottom_sheet, container, false)
     }
 
     companion object {
@@ -46,12 +46,12 @@ class OneLineReviewFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment OneLineReviewFragment.
+         * @return A new instance of fragment WriteBottomSheetFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            OneLineReviewFragment().apply {
+            WriteBottomSheetFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
