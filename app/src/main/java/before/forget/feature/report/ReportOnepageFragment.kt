22e487451d0bd316baca6c356be0e72b21647bc5
hjh1161514 <hjh1161514@naver.com>
2022-01-18 15:27:25 +0900

    [FEAT]: Reoprt Onepage Layout
    1. 통계뷰5 한눈에보기 전체뷰 레이아웃 구현
    2. 통계뷰5 한눈에보기 그래프 레이아웃 구현
    3. 통계뷰5 한눈에보기 그래프 구현

--- app/src/main/java/before/forget/feature/report/ReportOnepageFragment.kt
+++ app/src/main/java/before/forget/feature/report/ReportOnepageFragment.kt
@@ -1,61 +1,139 @@
 package before.forget.feature.report
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import before.forget.R
+import before.forget.databinding.FragmentReportOnepageBinding
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.ValueFormatter
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ReportOnepageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ReportOnepageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentReportOnepageBinding? = null
+    private val binding get() = _binding ?: error("Binding이 초기화되지 않았습니다.")
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var chart: BarChart? = null
+    private var MAX_X_VALUE = 5 // bar count
+    private var MAX_Y_VALUE = 0
+    private var COUNT_X_LABEL = 5
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report_onepage, container, false)
+        _binding = FragmentReportOnepageBinding.inflate(layoutInflater, container, false)
+
+        chart = binding.bcGraphOnepage
+
+        initBarChart()
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ReportOnepageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ReportOnepageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun initBarChart() {
+        val data: BarData = createBarChartData()
+        // set bar width
+        data.barWidth = 0.1f
+        configureChartAppearance()
+        prepareChartData(data)
+    }
+
+    private fun createBarChartData(): BarData {
+        val values: ArrayList<BarEntry> = ArrayList()
+        val record_count = arrayOf("8", "21", "25", "15", "18") // TODO: Server에서 받아오기
+        for (i in 0 until MAX_X_VALUE) {
+            val x = i.toFloat()
+            val y: Float = record_count[i].toFloat()
+            if (record_count[i].toInt() > MAX_Y_VALUE) MAX_Y_VALUE = record_count[i].toInt()
+            values.add(BarEntry(x, y))
+        }
+        val set1 = BarDataSet(values, "")
+        val dataSets: ArrayList<IBarDataSet> = ArrayList()
+        dataSets.add(set1)
+        // showing the value of the bar
+        set1.setDrawValues(false)
+        set1.barShadowColor = Color.BLACK
+        set1.color = Color.WHITE
+        return BarData(dataSets)
+    }
+
+    private fun configureChartAppearance() {
+        setChart()
+        setChartxAis()
+        setChartAxisLeftAndRight()
+    }
+
+    private fun setChart() {
+        chart!!.run {
+            description.isEnabled = false
+            setDrawValueAboveBar(false)
+            // hiding the grey background of the chart, default false if not set
+            setDrawGridBackground(false)
+            // x, y space
+            extraLeftOffset = 20f
+            extraRightOffset = 30f
+            extraTopOffset = 45f
+            extraBottomOffset = 45f
+
+            // X, Y 바의 애니메이션 효과
+            animateY(2000)
+            // bar background
+            setDrawBarShadow(true)
+            // bar touch
+            setTouchEnabled(false)
+            // chart label
+            getLegend().isEnabled = false
+        }
+    }
+
+    private fun setChartxAis() {
+        val xAxis = chart!!.xAxis
+        // xAxis position
+        xAxis.run {
+            position = XAxis.XAxisPosition.BOTTOM
+            textColor = Color.WHITE
+            labelCount = COUNT_X_LABEL
+            textSize = 14f
+
+            setDrawGridLines(false)
+            valueFormatter = object : ValueFormatter() {
+                override fun getFormattedValue(value: Float): String {
+                    val DAYS = arrayOf("8", "9", "10", "11", "12") // TODO: datepicker 기반으로 계산하기
+                    return DAYS[value.toInt()] + "월"
                 }
             }
+        }
+    }
+
+    private fun setChartAxisLeftAndRight() {
+        val axisLeft = chart!!.axisLeft
+        // axisLeft
+        axisLeft.run {
+            setLabelCount(3, true)
+            textColor = Color.WHITE
+            axisLineColor = Color.TRANSPARENT
+            textSize = 14f
+            // axisleft max, min
+            axisMinimum = 0f
+            axisMaximum = MAX_Y_VALUE.toFloat()
+        }
+        // y축 격자
+        // TODO : 축 색상 axisLeft.gridColor = Color
+        // 오른쪽 축 색상
+        val axisRight = chart!!.axisRight
+        axisRight.isEnabled = false
+    }
+
+    private fun prepareChartData(data: BarData) {
+        chart!!.data = data
+        chart!!.invalidate()
     }
 }
