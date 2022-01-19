7b60379b926821071faf60c7bbea7f896a74ad75
hjh1161514 <hjh1161514@naver.com>
2022-01-19 22:43:10 +0900

    [CHORE] Chagne Naming
    1. Labeling Data 이름 변경
    2. Service에 tempToken 적용3. Labeling Fragment에 tempToken 적용

--- app/src/main/java/before/forget/data/remote/api/StatisticService.kt
+++ app/src/main/java/before/forget/data/remote/api/StatisticService.kt
@@ -1,7 +1,8 @@
 package before.forget.data.remote.api
 
-import before.forget.data.remote.response.ReportLabelingData
+import before.forget.data.remote.response.ResponseLabelingData
 import before.forget.data.remote.response.ResponseWrapper
+import before.forget.data.remote.tempToken
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
@@ -10,7 +11,7 @@ import retrofit2.http.Path
 interface StatisticService {
     @GET("statistic/first/{date}")
     fun requestReportLabelingData(
-        @Header("accesstoken") token: String,
+        @Header("accesstoken") token: String? = tempToken,
         @Path("date") date: String,
-    ): Call<ResponseWrapper<ReportLabelingData>>
+    ): Call<ResponseWrapper<ResponseLabelingData>>
 }
