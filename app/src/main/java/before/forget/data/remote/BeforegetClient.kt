0360c40d5bb2ef0b3b5d557a6d0a4fd51a31b1ab
hjh1161514 <hjh1161514@naver.com>
2022-01-19 22:11:22 +0900

    [FEAT]: Remote Report Labeling
    1. StatisticServices 파일 추가
    2. Data 추가

--- app/src/main/java/before/forget/data/remote/BeforegetClient.kt
+++ app/src/main/java/before/forget/data/remote/BeforegetClient.kt
@@ -1,16 +1,21 @@
 package before.forget.data.remote
 
 import before.forget.data.remote.api.AuthService
+import before.forget.data.remote.api.StatisticService
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object BeforegetClient {
-    private const val BASE_URL = ""
+    private const val BASE_URL = "https://asia-northeast3-appjam-beforeget.cloudfunctions.net/api/"
 
     val authService: AuthService by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
         provideService(AuthService::class.java)
+    }
+
+    val statisticService: StatisticService by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
+        provideService(StatisticService::class.java)
     }
 
     private fun <T> provideService(clazz: Class<T>): T = Retrofit.Builder()
