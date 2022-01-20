b06d29543ae34f654d6dbd7e01f78d545727c3c9
hjh1161514 <hjh1161514@naver.com>
2022-01-20 21:09:50 +0900

    [FEAT]: Add Report Onepage Data
    1. ResponseOnepageData 추가
    2. StatisticService에 추가

---
+++ app/src/main/java/before/forget/data/remote/response/ResponseOnepageData.kt
@@ -0,0 +1,24 @@
+package before.forget.data.remote.response
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Path
+
+data class ResponseOnepageData(
+    val start: String,
+    val graphic: String,
+    val oneline: List<String>,
+    val monthly: List<GraphData>,
+    val media: List<SentenceData>
+)
+
+data class GraphData(
+    val count: Int,
+    val month: Int
+)
+
+data class SentenceData(
+    val type: String,
+    val count: Int
+)
