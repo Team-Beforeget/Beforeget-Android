a6e8bcb44561f584f6b62f4bf6efe536cbde2119
hjh1161514 <hjh1161514@naver.com>
2022-01-20 20:52:49 +0900

    [FEAT]: Remote CardFlipBack
    1. 카드 뒷면 한 줄 평 적용
    2. 카드 뒷면 엠티뷰 적용

--- app/src/main/java/before/forget/feature/report/CardFlipBack.kt
+++ app/src/main/java/before/forget/feature/report/CardFlipBack.kt
@@ -5,16 +5,44 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import before.forget.R
+import before.forget.databinding.ItemCardBackBinding
 
-class CardFlipBack : Fragment() {
+class CardFlipBack(var s1: String, var s2: String, var s3: String) : Fragment() {
+    private var _binding: ItemCardBackBinding? = null
+    private val binding get() = _binding ?: error("Binding이 초기화되지 않았습니다.")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = ItemCardBackBinding.inflate(layoutInflater, container, false)
+        initCardView()
+        return binding.root
+    }
 
-        return inflater.inflate(R.layout.item_card_back, container, false)
+    private fun initCardView() {
+        if (s1.isEmpty() && s2.isEmpty() && s3.isEmpty()) {
+            binding.ivSentenceIcon.visibility = View.VISIBLE
+            binding.tvSentenceEmpty.visibility = View.VISIBLE
+            binding.tvSentence1.visibility = View.INVISIBLE
+            binding.tvSentence2.visibility = View.INVISIBLE
+            binding.tvSentence3.visibility = View.INVISIBLE
+        } else {
+            binding.ivSentenceIcon.visibility = View.INVISIBLE
+            binding.tvSentenceEmpty.visibility = View.INVISIBLE
+            binding.tvSentence1.visibility = View.VISIBLE
+            binding.tvSentence2.visibility = View.VISIBLE
+            binding.tvSentence3.visibility = View.VISIBLE
+
+            binding.tvSentence1.text = s1
+            binding.tvSentence2.text = s2
+            binding.tvSentence3.text = s3
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
