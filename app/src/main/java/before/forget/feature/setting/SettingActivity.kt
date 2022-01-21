b20bbcc8c76d940b6724985faf1ebfbd108b41fc
hjh1161514 <hjh1161514@naver.com>
2022-01-21 22:50:35 +0900

    [FEAT]: Add Button Listnener to SettingActivity
    1. Setting Activity에 뒤로가기 버튼 리스너 추가

--- app/src/main/java/before/forget/feature/setting/SettingActivity.kt
+++ app/src/main/java/before/forget/feature/setting/SettingActivity.kt
@@ -2,11 +2,19 @@ package before.forget.feature.setting
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import before.forget.R
+import before.forget.databinding.ActivitySettingBinding
 
 class SettingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySettingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting)
+        initBtnClickListener()
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+    }
+
+    private fun initBtnClickListener() {
+        binding.btnSettingBack.setOnClickListener {
+            finish()
+        }
     }
 }
